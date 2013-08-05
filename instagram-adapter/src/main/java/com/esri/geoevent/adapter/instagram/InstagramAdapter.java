75bc930cf86a54c42467bf03abed5e885323f7a3
Hanoch Kalmanovich <hkalmanovich@esri.com>
2013-08-05 16:12:47 -0700

    Added copyright comment

--- instagram-adapter/src/main/java/com/esri/geoevent/adapter/instagram/InstagramAdapter.java
+++ instagram-adapter/src/main/java/com/esri/geoevent/adapter/instagram/InstagramAdapter.java
@@ -1,3 +1,27 @@
+/*
+  Copyright 1995-2013 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  For additional information, contact:
+  Environmental Systems Research Institute, Inc.
+  Attn: Contracts Dept
+  380 New York Street
+  Redlands, California, USA 92373
+
+  email: contracts@esri.com
+*/
+
 package com.esri.geoevent.adapter.instagram;
 
 import java.nio.ByteBuffer;
@@ -10,32 +34,38 @@ import com.esri.ges.adapter.InboundAdapterBase;
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.core.geoevent.GeoEvent;
 
-public class InstagramAdapter extends InboundAdapterBase {
-	private static final Log LOG = LogFactory.getLog(InstagramAdapter.class);
+public class InstagramAdapter extends InboundAdapterBase
+{
+  private static final Log LOG = LogFactory.getLog(InstagramAdapter.class);
 
-	public InstagramAdapter(AdapterDefinition definition)
-			throws ComponentException {
-		super(definition);
-		LOG.debug("Instagram Status Adapter created");
-	}
+  public InstagramAdapter(AdapterDefinition definition) throws ComponentException
+  {
+    super(definition);
+    LOG.debug("Instagram Status Adapter created");
+  }
 
-	public static void consoleDebugPrintLn(String msg) {
-		String consoleOut = System.getenv("GEP_CONSOLE_OUTPUT");
-		if (consoleOut != null && "1".equals(consoleOut)) {
-			System.out.println(msg);
-		}
-	}
+  public static void consoleDebugPrintLn(String msg)
+  {
+    String consoleOut = System.getenv("GEP_CONSOLE_OUTPUT");
+    if (consoleOut != null && "1".equals(consoleOut))
+    {
+      System.out.println(msg);
+    }
+  }
 
-	public static void consoleDebugPrint(String msg) {
-		String consoleOut = System.getenv("GEP_CONSOLE_OUTPUT");
-		if (consoleOut != null && "1".equals(consoleOut)) {
-			System.out.print(msg);
-		}
-	}
+  public static void consoleDebugPrint(String msg)
+  {
+    String consoleOut = System.getenv("GEP_CONSOLE_OUTPUT");
+    if (consoleOut != null && "1".equals(consoleOut))
+    {
+      System.out.print(msg);
+    }
+  }
 
-	@Override
-	protected GeoEvent adapt(ByteBuffer arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  protected GeoEvent adapt(ByteBuffer arg0, String arg1)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }
--- instagram-transport/src/main/java/com/esri/geoevent/transport/instagram/InstagramInboundTransport.java
+++ instagram-transport/src/main/java/com/esri/geoevent/transport/instagram/InstagramInboundTransport.java
@@ -1,16 +1,37 @@
+/*
+  Copyright 1995-2013 Esri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+  For additional information, contact:
+  Environmental Systems Research Institute, Inc.
+  Attn: Contracts Dept
+  380 New York Street
+  Redlands, California, USA 92373
+
+  email: contracts@esri.com
+ */
+
 package com.esri.geoevent.transport.instagram;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpRequest;
-import org.apache.http.params.HttpParams;
 
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.core.component.RunningState;
 import com.esri.ges.transport.TransportContext;
 import com.esri.ges.transport.TransportDefinition;
 import com.esri.ges.transport.http.HttpInboundTransport;
-import com.esri.ges.transport.http.HttpTransportContext;
 
 public class InstagramInboundTransport extends HttpInboundTransport
 {
@@ -21,7 +42,7 @@ public class InstagramInboundTransport extends HttpInboundTransport
 
   public InstagramInboundTransport(TransportDefinition definition) throws ComponentException
   {
-    super(definition);		
+    super(definition);
   }
 
   @Override
@@ -30,7 +51,7 @@ public class InstagramInboundTransport extends HttpInboundTransport
     super.start();
     logger.debug("Instagram-Inbound started.");
   }
-  
+
   @Override
   public synchronized void stop()
   {
@@ -57,20 +78,20 @@ public class InstagramInboundTransport extends HttpInboundTransport
   {
     logger.debug("Inbound Skip validation...");
   }
-   
+
   @Override
   public void onReceive(TransportContext context)
   {
     super.onReceive(context);
     consoleDebugPrintLn("onReceive");
   }
-  
+
   @Override
   public synchronized boolean isRunning()
   {
     return (getRunningState() == RunningState.STARTED);
   }
-  
+
   public void applyProperties() throws Exception
   {
     if (getProperty("client_id").isValid())
@@ -89,9 +110,9 @@ public class InstagramInboundTransport extends HttpInboundTransport
         clientSecret = value;
       }
     }
-	clientUrl += "?client_id=" + clientId + "&client_secret=" + clientSecret;
+    clientUrl += "?client_id=" + clientId + "&client_secret=" + clientSecret;
   }
-  
+
   public static void consoleDebugPrintLn(String msg)
   {
     String consoleOut = System.getenv("GEP_CONSOLE_OUTPUT");
